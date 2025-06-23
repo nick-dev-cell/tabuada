53ca60450002276746fe5b5bfc1f91cdd3075dc9
nick-dev-cell <nickollas.k.santos@gmail.com>
2025-06-23 20:29:35 -0300

    Add files via upload

---
+++ tabuada-main/tabuada-main/script.js
@@ -0,0 +1,63 @@
+/* var res = document.querySelector(".res")
+
+var numero = Number(prompt("qual tabuada vc quer saber"))
+for(var inicio = 1; inicio <= 10; inicio++){
+    res.innerHTML += `<p>${numero} x ${inicio}</p>`
+}
+ */
+
+var semana = Number(prompt("digite o dia da semana de 1 a 7")) 
+
+switch(semana){
+    case 1:
+        alert("domingo")
+        break;
+        case 2:
+            alert("segunda")
+            break;
+            case 3:
+                alert("terca")
+                break;
+                case 4:
+                    alert("quarta")
+                    break;
+                    case 5:
+                    alert("quinta")
+                    break;
+                    case 6:
+                    alert("sexta")
+                    break;
+                    case 7:
+                    alert("sabado")
+                    break;
+                    default:
+                        alert("erro")
+                    
+
+}
+
+
+
+
+
+
+
+
+/* formas de incremento */
+
+/* numero = numero + 1 */
+/* numero += 1 */
+/* .innerHTML */
+/* .innertext
+ */
+/* numero++ */
+/* 
+tipos de function
+
+()=> : arrow function
+
+funnction()
+
+function{  : function declarada
+}
+ */
